7b940b55349c2854ce16c532b63e09bec778e857
Mauko Maunde <maukoese@gmail.com>
2019-08-29 02:38:33 +0300

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -3,15 +3,14 @@
  * Register autoloader for classes under the Osen namespace
  * @param class $class Full namespaced class e.g Osen\Coop\TransactionStatus
  */
-spl_autoload_register(function ($class)
-{
+spl_autoload_register(function ($class) {
     if (strpos($class, 'Osen\Coop')) {
-        $class  = str_replace('Osen\Coop', '', $class);
-        $path   = str_replace('\\', '/', $class);
-        require_once __DIR__."/src/{$path}.php";
+        $class = str_replace('Osen\Coop', '', $class);
+        $path  = str_replace('\\', '/', $class);
+        require_once __DIR__ . "/src/{$path}.php";
     }
 });
 /**
  * Load helper functions for more concise code
  */
-require_once('src/helpers.php');
+require_once __DIR__ . '/src/helpers.php';
