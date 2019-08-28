96962e9bf4e042b97ee00a15625ebf3556f94608
Mauko Maunde <maukoese@gmail.com>
2019-08-29 00:34:00 +0300

    Create autoload.php

---
+++ autoload.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Register autoloader for classes under the Osen namespace
+ * @param class $class Full namespaced class e.g Osen\Coop\TransactionStatus
+ */
+spl_autoload_register(function ($class)
+{
+    if (strpos($class, 'Osen\Coop')) {
+        $class  = str_replace('Osen\Coop', '', $class);
+        $path   = str_replace('\\', '/', $class);
+        require_once "src/{$path}.php";
+    }
+});
+/**
+ * Load helper functions for more concise code
+ */
+require_once('src/helpers.php');
