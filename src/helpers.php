1c2fdc20f441a24a625107c038a0da4fe05256b8
Mauko Maunde <maukoese@gmail.com>
2019-09-01 07:56:25 +0300

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -1,7 +1,7 @@
 <?php
 
-if (!function_exists('setup_coop')) {
-    function setup_coop($config)
+if (!function_exists('coopSetup')) {
+    function coopSetup($config)
     {
         Osen\Coop\AccountBalance::init($config);
         Osen\Coop\AccountTransactions::init($config);
@@ -12,43 +12,43 @@ if (!function_exists('setup_coop')) {
     }
 }
 
-if (!function_exists('CoopAccountBalance')) {
-    function CoopAccountBalance($MessageReference, $AccountNumber = null, $callback = null)
+if (!function_exists('coopAccountBalance')) {
+    function coopAccountBalance($MessageReference, $AccountNumber = null, $callback = null)
     {
         return Osen\Coop\AccountBalance::send($MessageReference, $AccountNumber, $callback);
     }
 }
 
-if (!function_exists('CoopAccountTransactions')) {
-    function CoopAccountTransactions($MessageReference, $AccountNumber, $NoOfTransactions = '1', $callback = null)
+if (!function_exists('coopAccountTransactions')) {
+    function coopAccountTransactions($MessageReference, $AccountNumber, $NoOfTransactions = '1', $callback = null)
     {
         return Osen\Coop\AccountTransactions::send($MessageReference, $AccountNumber, $NoOfTransactions, $callback);
     }
 }
 
-if (!function_exists('CoopExchangeRate')) {
-    function CoopExchangeRate($MessageReference, $FromCurrencyCode = 'KES', $ToCurrencyCode = 'USD', $callback = null)
+if (!function_exists('coopExchangeRate')) {
+    function coopExchangeRate($MessageReference, $FromCurrencyCode = 'KES', $ToCurrencyCode = 'USD', $callback = null)
     {
         return Osen\Coop\ExchangeRate::send($MessageReference, $FromCurrencyCode, $ToCurrencyCode, $callback);
     }
 }
 
-if (!function_exists('CoopIFTAccountToAccount')) {
-    function CoopIFTAccountToAccount($MessageReference, $AccountNumber, $Amount, $TransactionCurrency = 'KES', $Narration = 'Payment', $Destinations = array(), $callback = null)
+if (!function_exists('coopIFTAccountToAccount')) {
+    function coopIFTAccountToAccount($MessageReference, $AccountNumber, $Amount, $TransactionCurrency = 'KES', $Narration = 'Payment', $Destinations = array(), $callback = null)
     {
         return Osen\Coop\IFTAccountToAccount::send($MessageReference, $AccountNumber, $Amount, $TransactionCurrency, $Narration, $Destinations, $callback);
     }
 }
 
-if (!function_exists('CoopPesaLinkSendToAccount')) {
-    function CoopPesaLinkSendToAccount($MessageReference, $AccountNumber, $Amount, $TransactionCurrency = 'KES', $Narration = 'Payment', $Destinations = array(), $callback = null)
+if (!function_exists('coopPesaLinkSendToAccount')) {
+    function coopPesaLinkSendToAccount($MessageReference, $AccountNumber, $Amount, $TransactionCurrency = 'KES', $Narration = 'Payment', $Destinations = array(), $callback = null)
     {
         return Osen\Coop\PesaLinkSendToAccount::send($MessageReference, $AccountNumber, $Amount, $TransactionCurrency, $Narration, $Destinations, $callback);
     }
 }
 
-if (!function_exists('CoopTransactionStatus')) {
-    function CoopTransactionStatus($MessageReference, $callback = null)
+if (!function_exists('coopTransactionStatus')) {
+    function coopTransactionStatus($MessageReference, $callback = null)
     {
         return Osen\Coop\TransactionStatus::send($MessageReference, $callback);
     }
